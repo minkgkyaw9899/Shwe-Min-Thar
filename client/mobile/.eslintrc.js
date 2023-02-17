de008b66e8da38f0f1ac6128c5a97ab512010ec0
minkgkyaw9899 <minkgkyaw9899@gmail.com>
2023-02-17 19:38:24 +0630

    feat: setup buyer app

--- client/mobile/.eslintrc.js
+++ client/mobile/.eslintrc.js
@@ -1,4 +1,40 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+}
